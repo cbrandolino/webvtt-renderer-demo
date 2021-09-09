26dce187f8dadecd8f7bd69809d0802a7e4b07cc
Claudio Brandolino <claudio.brandolino@wmcontractor.com>
2021-09-09 13:42:47 +0200

    Transform settings into proper typescript

--- src/vttjs/Settings.ts
+++ src/vttjs/Settings.ts
@@ -1,58 +1,63 @@
 // A settings object holds key/value pairs and will ignore anything but the first
 // assignment to a specific key.
-function Settings() {
-  this.values = Object.create(null);
+interface ISettings {
+  values: Object;
+  set: (k: string, v: unknown) => void;
 }
 
-Settings.prototype = {
-  // Only accept the first assignment to any key.
-  set: function(k, v) {
+class Settings implements ISettings {
+  values = Object.create(null);
+
+  set(k:string, v:unknown) {
     if (!this.get(k) && v !== "") {
       this.values[k] = v;
     }
-  },
+  }
+
   // Return the value for a key, or a default value.
   // If 'defaultKey' is passed then 'dflt' is assumed to be an object with
   // a number of possible default values as properties where 'defaultKey' is
   // the key of the property that will be chosen; otherwise it's assumed to be
   // a single value.
-  get: function(k, dflt, defaultKey) {
-    if (defaultKey) {
-      return this.has(k) ? this.values[k] : dflt[defaultKey];
-    }
-    return this.has(k) ? this.values[k] : dflt;
-  },
+  get(k:string, dflt?:{[key:string]:unknown}|string, defaultKey?:string) {
+    const defaultValue = (defaultKey && (dflt instanceof Object)) ? dflt[defaultKey] : dflt;
+    return this.has(k) ? this.values[k] : defaultValue;
+  }
+
   // Check whether we have a value for a key.
-  has: function(k) {
+  has(k:string) {
     return k in this.values;
-  },
+  }
+
   // Accept a setting if its one of the given alternatives.
-  alt: function(k, v, a) {
+  alt(k:string, v:unknown, a:Array<unknown>) {
     for (var n = 0; n < a.length; ++n) {
       if (v === a[n]) {
         this.set(k, v);
         break;
       }
     }
-  },
+  }
+
   // Accept a setting if its a valid (signed) integer.
-  integer: function(k, v) {
+  integer(k:string, v:string) {
     if (/^-?\d+$/.test(v)) { // integer
       this.set(k, parseInt(v, 10));
     }
-  },
+  }
+
   // Accept a setting if its a valid percentage.
-  percent: function(k, v) {
-    var m;
-    if ((m = v.match(/^([\d]{1,3})(\.[\d]*)?%$/))) {
-      v = parseFloat(v);
-      if (v >= 0 && v <= 100) {
-        this.set(k, v);
+  percent(k:string, v:string) {
+    if ((v.match(/^([\d]{1,3})(\.[\d]*)?%$/))) {
+      const vFloat = parseFloat(v);
+      if (vFloat >= 0 && vFloat <= 100) {
+        this.set(k, vFloat);
         return true;
       }
     }
     return false;
   }
-};
+  
+}
 
 export default Settings;
